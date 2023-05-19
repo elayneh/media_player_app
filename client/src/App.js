61e8ee935f1e551d40141e2eee85216c61eb1165
elayneh <betenatefera@gmail.com>
2023-05-19 17:44:18 +0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import Container from "./Components/Container";
+import { ThemeProvider } from "styled-components";
+
+const theme = {};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ThemeProvider theme={theme}>{/* <Container /> */}
+  <h1>
+    MEDIA PLAYER APP
+
+    </h1></ThemeProvider>;
 }
 
-export default App;
+export default App
