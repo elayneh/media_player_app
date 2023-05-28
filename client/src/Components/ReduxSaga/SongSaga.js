020c749d1738fa6da517081153f2e55f9128801f
belayneh getachew <87185500+elayneh@users.noreply.github.com>
2023-05-28 21:25:06 +0300

    Delete client directory

--- client/src/Components/ReduxSaga/SongSaga.js
+++
@@ -1,7 +0,0 @@
-import { all } from "redux-saga/effects";
-import axios from "axios";
-import { wactchSongAsync } from "./Types/Sagas";
-
-export function* rootSaga() {
-  yield all([wactchSongAsync()]);
-}
--- client/src/Components/ReduxSaga/Types/ActionTypes.js
+++
@@ -1,4 +0,0 @@
-export const ADD_SONG = "ADD_SONG";
-export const GET_SONG = "GET_SONG";
-export const UPDATE_SONG = "UPDATE_SONG";
-export const DELETE_SONG = "DELETE_SONG";
